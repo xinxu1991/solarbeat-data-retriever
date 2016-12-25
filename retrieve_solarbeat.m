dd4ac57e8297fb1dd598d3cb2af41ea0197280a1
xinxu1991 <xuxin.cqu@live.com>
2016-12-25 17:44:15 +0100

    Add support to early versions of MATLAB that don't support timetable

--- retrieve_solarbeat.m
+++ retrieve_solarbeat.m
@@ -4,11 +4,13 @@ Name:   retrieve_solarbeat.m
 
 Version history:
     2016-12-11: v0.1, initial version
+    2016-12-11: v0.1-l, add support to early versions of MATLAB that don't
+    support timetable
 
 Author: Xin Xu (xuxin.cqu@live.com)
 
 Required:
-    1. MATLAB R2016b or newer
+    1. MATLAB
     2. MATLAB database toolbox
     (https://www.mathworks.com/products/database.html)
     3. Credentials of SolarBEAT server (may be requested from dr. Roland
@@ -24,8 +26,9 @@ Purpose:
 %}
 
 %% parameters
-starttime = '00:00:00';
-endtime = '23:59:59';
+daystart = '00:00:00';
+dayend = '23:59:59';
+sample_rate = minutes(1);
 querry_limit = 5000;
 sd = '[SensorData]';
 sdts = 'SensorDataTimeStamp';
@@ -33,60 +36,45 @@ sdtsquotes = '[SensorDataTimeStamp]';
 sdval = '[SensorDataValue]';
 
 %% inputs
+starttime = datetime([startday,' ',daystart],'InputFormat','yyyy-MM-dd HH:mm:ss');
+endtime = datetime([endday,' ',dayend],'InputFormat','yyyy-MM-dd HH:mm:ss');
 startday = datetime(startday,'InputFormat','yyyy-MM-dd');
 endday = datetime(endday,'InputFormat','yyyy-MM-dd');
 
 %% retrieve
+% timestamp
+data{1} = transpose(starttime:sample_rate:endtime);
 for i=1:1:length(db)
     % connect to database
     conn = database(db(i).name,db(i).username,db(i).password,...
         'Vendor','Microsoft SQL Server','Server',host,...
         'AuthType','Server','PortNumber',1433);
     today = startday;
-    data{i} = [];
-    k = 1;
-    
-    while today <= endday
-        % construct daily timetable
-        Time = transpose(datetime(datevec(today)):minutes(1):datetime(datevec(today+hours(23)+minutes(59))));
-        Index = transpose(k:1:k-1+length(Time));
-        daily_table = timetable(Time,Index);
-        k = k+length(Time);
-        clear Time Index;
-        
+    data{i+1} = nan(length(data{1}),length(db(i).sensor));
+   
+    while today <= endday      
         % retrieve from server
         for j=1:1:length(db(i).sensor)
             % compose querry
             str_today = datestr(today,'yyyy-mm-dd');
             from_query = ['[', db(i).name, '].', sd, '.[', db(i).sensor{j}, ']'];
-            longstring_start = [char(39), str_today, ' ', starttime, char(39)];
-            longstring_end = [char(39), str_today, ' ', endtime, char(39)];
+            longstring_start = [char(39), str_today, ' ', daystart, char(39)];
+            longstring_end = [char(39), str_today, ' ', dayend, char(39)];
             date_query = [sdts, ' >= ', longstring_start, ' AND ', sdts, ' <= ', longstring_end];
             total_query = ['SELECT TOP ', num2str(querry_limit), ' * FROM ', from_query, ' WHERE ', date_query];
             % execute querry
             curs = exec(conn,total_query);
             fetched = fetch(curs);
             % write to daily timetable
-            if isequal(fetched.Data,{'No Data'}) % whether 'No Data'
-                Time = transpose(datetime(datevec(today)):minutes(1):datetime(datevec(today+hours(23)+minutes(59))));
-                Data = nan(length(Time),1); % write NaN to daily timetable
-                temp_table = timetable(Time,Data);
-                temp_table.Properties.VariableNames = {db(i).variable{j}};
-                daily_table = synchronize(daily_table,temp_table);
-                % clear temp
-                clear Time Data temp_table;
-            else
+            if ~isequal(fetched.Data,{'No Data'}) % whether 'No Data'
                 Time = datetime(cell2mat(fetched.Data(:,2)),'InputFormat','yyyy-MM-dd HH:mm:ss.0');
                 Data = cell2mat(fetched.Data(:,3)); % write fetched data to daily timetable
-                temp_table = timetable(Time,Data);
-                temp_table.Properties.VariableNames = {db(i).variable{j}};
-                daily_table = synchronize(daily_table,temp_table);
+                Index = round((Time-starttime)/sample_rate)+1;
+                data{i+1}(Index,j) = Data;
                 % clear temp
-                clear Time Data temp_table;
+                clear Time Data Index;
             end
         end
-        % concatenate daily timetables
-        data{i} = [data{i}; daily_table];
         today = today + days(1);
     end
     % close connection
